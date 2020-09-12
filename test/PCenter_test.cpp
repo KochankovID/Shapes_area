6303fc68da48386395b3d6a846adf7d048372573
Ilya Kochankov <ilyakochankov@MBP-Ilya.Dlink>
2020-09-12 21:55:18 +0300

    PCenter tested

--- test/PCenter_test.cpp
+++ test/PCenter_test.cpp
@@ -15,3 +15,53 @@ TEST(PCenter_construct, default_construct_correct){
     ASSERT_EQ(pCenter.x(), 0);
     ASSERT_EQ(pCenter.y(), 0);
 }
+
+TEST(PCenter_construct, point_construct_no_throw){
+    ASSERT_NO_THROW(PCenter(Point(2, 3)));
+}
+
+TEST(PCenter_construct, point_construct_correct){
+    PCenter pCenter(Point(4, 5));
+
+    ASSERT_EQ(pCenter.x(), 4);
+    ASSERT_EQ(pCenter.y(), 5);
+}
+
+TEST(PCenter_construct, x_y_construct_no_throw){
+    ASSERT_NO_THROW(PCenter(2, 3));
+}
+
+TEST(PCenter_construct, x_y_construct_correct){
+    PCenter pCenter(7, 1);
+
+    ASSERT_EQ(pCenter.x(), 7);
+    ASSERT_EQ(pCenter.y(), 1);
+}
+
+TEST(PCenter_construct, copy_construct_no_throw){
+    ASSERT_NO_THROW(PCenter(2, 3));
+}
+
+TEST(PCenter_construct, copy_construct_correct){
+    PCenter pCenter1(7, 1);
+
+    PCenter pCenter(pCenter1);
+
+    ASSERT_EQ(pCenter.x(), 7);
+    ASSERT_EQ(pCenter.y(), 1);
+}
+
+TEST(PCenter_assignment, assign_operator_no_throw){
+    PCenter a, b(1,2);
+
+    ASSERT_NO_THROW(a = b);
+}
+
+TEST(PCenter_assignment, assign_operator_correct){
+    PCenter a, b(1,2);
+
+    a = b;
+
+    ASSERT_EQ(a.x(), 1);
+    ASSERT_EQ(a.y(), 2);
+}
