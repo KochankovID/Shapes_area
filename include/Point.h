ed26419e0c37cd959d8390c531cedc58703f5c8e
Ilya Kochankov <ilyakochankov@MBP-Ilya.Dlink>
2020-09-12 11:56:17 +0300

    init + class Point

---
+++ include/Point.h
@@ -0,0 +1,22 @@
+//
+// Created by Ilya Kochankov on 9/12/20.
+//
+
+#ifndef SHAPES_AREA_POINT_H
+#define SHAPES_AREA_POINT_H
+
+#include "utility"
+
+struct Point{
+    Point(double x, double y);
+    Point(const Point& copy);
+
+    Point& operator=(const Point& copy);
+
+    virtual ~Point();
+
+    double x;
+    double y;
+};
+
+#endif //SHAPES_AREA_POINT_H
---
+++ src/Point.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Ilya Kochankov on 9/12/20.
+//
+
+#include "../include/Point.h"
+
+Point::Point(double x, double y) {
+    this->x = x;
+    this->y = y;
+}
+
+Point::Point(const Point &copy) {
+    this->x = copy.x;
+    this->y = copy.y;
+}
+
+Point::~Point() {
+
+}
+
+Point &Point::operator=(const Point &copy) {
+    this->x = copy.x;
+    this->y = copy.y;
+    return *this;
+}
